f974ee59adbf11aa48621716b0ad3a3a4427c96c
zaidkhazaleh <wrkngrly84@gmail.com>
2024-11-25 00:35:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-<script>
+// منع النقر بزر الفأرة الأيمن
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
+// منع اختصارات لوحة المفاتيح
 document.addEventListener('keydown', function(event) {
     if (event.key === 'F12' || 
         (event.ctrlKey && event.shiftKey && event.key === 'I') || 
         (event.ctrlKey && event.shiftKey && event.key === 'J') || 
         (event.ctrlKey && event.key === 'U')) {
         event.preventDefault();
+        alert('تم تعطيل هذه الاختصارات لأسباب أمنية.');
     }
 });
 
+// تحذير عند فتح أدوات المطور
 const element = new Image();
 Object.defineProperty(element, 'id', {
     get: function() {
@@ -20,9 +23,8 @@ Object.defineProperty(element, 'id', {
 });
 console.log(element);
 
+// إظهار القائمة المنسدلة
 function toggleDropdown() {
     const dropdown = document.querySelector('.dropdown');
     dropdown.style.display = dropdown.style.display === 'none' || dropdown.style.display === '' ? 'grid' : 'none';
 }
-
-</script>
